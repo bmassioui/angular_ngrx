76e9c49ab8244f6ea4004c693f7cd2edd3ef0c45
bmassioui <bouchaib.massioui@gmail.com>
2022-03-04 11:02:55 +0000

    undo the changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookListComponent } from './_components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BookListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
