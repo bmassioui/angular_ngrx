50924ed7cb014f78272935941296e1ebb087bb36
bmassioui <bouchaib.massioui@gmail.com>
2022-03-04 16:05:23 +0000

    build the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { GoogleBooksService } from './book-list/books.service';
+import { addBook, removeBook, retrieveBookList } from './state/books.actions';
+import { selectBookCollection, selectBooks } from './state/books.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular_Ngrx';
+export class AppComponent implements OnInit {
+  title = 'Angular Ngrx';
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  constructor(private booksService: GoogleBooksService, private store: Store) { }
+
+  /**
+   * Add Book
+   * @param bookId 
+   */
+  onAdd(bookId: string) {
+    this.store.dispatch(addBook({ bookId }));
+  }
+
+  /**
+   * Remove Book
+   * @param bookId 
+   */
+  onRemove(bookId: string) {
+    this.store.dispatch(removeBook({ bookId }));
+  }
+
+  ngOnInit(): void {
+    this.booksService.getBooks()
+      .subscribe((books) => this.store.dispatch(retrieveBookList({ books })));
+  }
 }
