1cd04032b2657ab3981e791f73c48955471085bf
bmassioui <bouchaib.massioui@gmail.com>
2022-03-04 13:13:11 +0000

    build book-collection component

--- src/app/book-list/_components/book-collection/book-collection.component.ts
+++ src/app/book-list/_components/book-collection/book-collection.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Book } from '../../books.model';
 
 @Component({
   selector: 'app-book-collection',
   templateUrl: './book-collection.component.html',
   styleUrls: ['./book-collection.component.css']
 })
-export class BookCollectionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BookCollectionComponent  {
+  @Input() books: ReadonlyArray<Book> = [];
+  @Output() remove = new EventEmitter<string>();
 }
